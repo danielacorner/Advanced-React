6584aa54ae99c76e093a1bdbfbb2cf977b087a08
dcorn068 <dcorn068@users.noreply.github.com>
2018-10-11 16:34:54 -0400

    first 7 videos

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
+
+const Nav = () => {
+  return (
+    <NavStyles>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/sell">
+        <a>Sell</a>
+      </Link>
+    </NavStyles>
+  );
+};
+
+export default Nav;
