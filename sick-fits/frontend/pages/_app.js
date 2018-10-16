67207235423cbc9c75b0d0c343202988117ea197
Daniel Corner <dcorn068@gmail.com>
2018-10-16 15:07:43 -0400

    Set up Prisma Yoga flow and Apollo Client

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -3,14 +3,21 @@ import Page from '../components/Page';
 import { ApolloProvider } from 'react-apollo';
 import withData from '../lib/withData';
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the users
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
   render() {
-    const { Component, apollo } = this.props;
-
-    return (
+    const { Component, apollo, pageProps } = this.props;
       <Container>
-        <ApolloProvider client={apollo}>
+          <ApolloProvider client={apollo}>
           <Page>
-            <Component />
+            <Component {...pageProps} />
           </Page>
         </ApolloProvider>
       </Container>
