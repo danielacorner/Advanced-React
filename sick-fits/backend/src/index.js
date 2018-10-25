fa6850982b013c64b3fa39c3675a3ecdd98aba27
dcorn068 <dcorn068@users.noreply.github.com>
2018-10-25 15:04:00 -0400

    add sign in form, sign out button, back-end password reset

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
@@ -8,7 +9,18 @@ const server = createServer();
 
 // Use express middlware to handle cookies (JWT)
 server.express.use(cookieParser());
-// TODO Use express middlware to populate current user
+
+// Decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests
+    req.userId = userId;
+  }
+
+  next();
+});
 
 server.start(
   {
